c0bc87c9305d3796992905188949ce01b8e641c7
Prinksm <prinkal.mahajan@argusoft.com>
2025-08-11 17:24:07 +0530

    socket assign

--- com/argusoft/exercise/implementation/socket/ServerSide.java
+++ com/argusoft/exercise/implementation/socket/ServerSide.java
@@ -5,29 +5,37 @@ import java.net.*;
 public class ServerSide {
     private Socket s = null;
     private ServerSocket server_soc = null;
+    //for reading data
     private DataInputStream in = null;
+    //for sending data
     private DataOutputStream op = null;
 
     public ServerSide(int port) throws IOException{
+        //creates server socket
         server_soc = new ServerSocket(port);
         System.out.println("Server started");
         System.out.println("Waiting for a client ...");
+        //wait for client to accept call
         s = server_soc.accept();
         System.out.println("ClientSide accepted");
+        //buffered used so that data s read in chunks
         in = new DataInputStream(new BufferedInputStream(s.getInputStream()));
         op = new DataOutputStream(s.getOutputStream());
 
     }
 
     public void start() throws IOException{
+        //print writer is used to write data in client file
         PrintWriter writer = new PrintWriter("/home/prinkalm@id.argusoft.com/prinks/client.txt");
         String mess = " ";
         while(true){
+            //reads client mess
             mess = in.readUTF();
             if(mess.equalsIgnoreCase("over")){
                 break;
             }
             System.out.println("client message : "+mess);
+            //echo the client message
             op.writeUTF("ECHO : "+mess);
             writer.println(mess);
             writer.flush();
